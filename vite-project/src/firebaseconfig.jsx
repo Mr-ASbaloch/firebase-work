4b69452ad9c40e077437355fee3419815e7434af
Mr-ASbaloch <abdulsaeed1356@gmail.com>
2023-08-17 05:19:42 -0700

    commit

---
+++ vite-project/src/firebaseconfig.jsx
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDncacZKM2LlRP2K2rsM-m2v8tzBdsZczI",
+  authDomain: "fir-app-cf930.firebaseapp.com",
+  projectId: "fir-app-cf930",
+  storageBucket: "fir-app-cf930.appspot.com",
+  messagingSenderId: "539834700504",
+  appId: "1:539834700504:web:34fae73febd9885c017dad",
+  measurementId: "G-QSYYR3MZLV"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
