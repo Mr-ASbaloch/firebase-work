8dea2e83fc6c2b6920b5b29e75490043837cb6be
Mr-ASbaloch <abdulsaeed1356@gmail.com>
2023-09-06 15:07:12 +0500

    commit

--- admin-dashboard/src/User/User.jsx
+++ admin-dashboard/src/User/User.jsx
@@ -1,9 +1,124 @@
-import React from 'react'
+import { Button } from "antd";
+import React, { useState } from "react";
+import { doc, setDoc } from "firebase/firestore"; 
+import { db } from "../firebase";
 
 const User = () => {
-  return (
-    <div>User</div>
-  )
-}
+const [name,setName] =useState("")
+const[email,setEmail] =useState ("")
+const [phone, setPhone]=useState("")
+const [address, setAddress]=useState (" ")
+const [password ,setPassword]=useState ("")
+const [country, setCountry] =useState ("")
 
-export default User
+    const handleData = async(e)=>{
+        e.preventDefault()
+    const data= await setDoc(doc(db, "cities", "LA"), {
+    name: "Los Angeles",
+    state: "CA",
+    country: "USA"
+  });
+  console.log(data);
+    }
+  return (
+    <div>
+  
+      <form
+        onSubmit={handleData}
+        className="text-[#00204a] mt-11 rounded-md font-semibold bg-gray-300 h-auto pb-8 pt-5 mx-auto  justify-center  w-[90%] lg:w-[50%]  flex flex-col "
+      >
+        <label
+          htmlFor="user name"
+          className="ml-[10%] text-xl capitalize mb-3 mt-3 "
+        >
+          name :
+        </label>
+        <input
+          type="text"
+          required
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+          className="w-[80%] lg:w-[50%] flex mx-auto bg-[#fafafa]  text-black text-center outline-none p-2 rounded-md"
+        />
+        <label
+          htmlFor="user email"
+          className="ml-[10%] capitalize text-xl mb-3 mt-3 "
+        >
+          email :
+        </label>
+        <input
+          type="email"
+          required
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+          className="w-[80%] lg:w-[50%] flex mx-auto bg-[#fafafa] outline-none p-2 rounded-md text-black text-center  "
+        />
+        <label
+          htmlFor="user email"
+          className="ml-[10%] capitalize text-xl mb-3 mt-3 "
+        >
+          Phone No :
+        </label>
+        <input
+          type="number"
+          required
+          onChange={(e) => {
+            setPhone(e.target.value);
+          }}
+          className="w-[80%] lg:w-[50%] flex mx-auto bg-[#fafafa] outline-none p-2 rounded-md text-black text-center  "
+        />
+        <label
+          htmlFor="user password"
+          className="ml-[10%] capitalize  text-xl mb-3 mt-3 "
+        >
+          password :
+        </label>
+        <input
+          type="password"
+          required
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+          className="w-[80%] lg:w-[50%] flex mx-auto outline-none rounded-md bg-[#fafafa] text-black text-center  p-2"
+        />
+        <label
+          htmlFor="user password"
+          className="ml-[10%] capitalize  text-xl mb-3 mt-3 "
+        >
+          address:
+        </label>
+        <input
+          type="password"
+          required
+          onChange={(e) => {
+            setAddress(e.target.value);
+          }}
+          className="w-[80%] lg:w-[50%] flex mx-auto outline-none rounded-md bg-[#fafafa] text-black text-center  p-2"
+        />
+        <label
+          htmlFor="user password"
+          className="ml-[10%] capitalize  text-xl mb-3 mt-3 "
+        >
+          country :
+        </label>
+        <input
+          type="password"
+          required
+          onChange={(e) => {
+            setCountry(e.target.value);
+          }}
+          className="w-[80%] lg:w-[50%] flex mx-auto outline-none rounded-md bg-[#fafafa] text-black text-center  p-2"
+        />
+
+        <Button type="primary" className="bg-green-700 w-[30%] mx-auto mt-5" onClick={handleData}>
+          {" "}
+          Add User
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+export default User;
--- admin-dashboard/src/firebase.js
+++ admin-dashboard/src/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyADd2F7fwWSQBgWeSTbO4EiOPFircY15xQ", 
@@ -13,4 +14,5 @@ const firebaseConfig = {
 
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+export const auth = getAuth(app); 
+ export const db = getFirestore(app);
